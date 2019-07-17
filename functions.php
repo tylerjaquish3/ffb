2b9d8b9e894c892304348aac18d8804c77ab8343
Tyler Jaquish <tjaquish@stepstoneglobal.com>
2019-07-17 15:39:33 -0700

    Parse playoffs and added dynamic pages.

--- functions.php
+++ functions.php
@@ -12,3 +12,80 @@ $conn = new mysqli($servername, $username, $password, $dbname, '3307');
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
+
+$dashboardNumbers = getDashboardNumbers($conn);
+$profileName = getProfileName($conn);
+
+
+function getDashboardNumbers($conn)
+{
+    $response = [];
+
+    $result = mysqli_query($conn,"select count(distinct(year)) as num_years from finishes");
+    while($row = mysqli_fetch_array($result)) 
+    {
+        $response['seasons'] = $row['num_years'];
+    }
+
+    $result = mysqli_query($conn,"SELECT count(distinct(manager_id)) as winners FROM finishes WHERE finish = 1");
+    while($row = mysqli_fetch_array($result)) 
+    {
+        $response['unique_winners'] = $row['winners'];
+    }
+
+    $result = mysqli_query($conn, "SELECT MAX(championships) as championships FROM (SELECT count(manager_id) as championships FROM finishes WHERE finish = 1 group by manager_id ORDER BY championships DESC LIMIT 1) as max_num");
+    while($row = mysqli_fetch_array($result)) 
+    {
+        $response['most_championships_number'] = $row['championships'];
+    }
+
+    $tempName = '';
+    $result = mysqli_query($conn, "SELECT count(manager_id) as championships, name FROM finishes JOIN managers on managers.id = finishes.manager_id  WHERE finish = 1 GROUP BY name HAVING count(manager_id) = ".$response['most_championships_number']);
+    while($row = mysqli_fetch_array($result)) 
+    {
+        if ($tempName == '') {
+            $tempName = $row['name'];
+        } else {
+            $tempName .= ', '.$row['name'];
+        }
+    }
+
+    $response['most_championships_manager'] = $tempName;
+
+    $result = mysqli_query($conn, "SELECT count(manager1_id) as wins FROM regular_season_matchups rsm WHERE manager1_score > manager2_score GROUP BY manager1_id ORDER BY count(manager1_id) DESC LIMIT 1");
+    while($row = mysqli_fetch_array($result)) 
+    {
+        $response['most_wins_number'] = $row['wins'];
+    }
+
+    $tempName = '';
+    $result = mysqli_query($conn, "SELECT count(manager1_id) as championships, name FROM regular_season_matchups rsm JOIN managers on managers.id = rsm.manager1_id   WHERE manager1_score > manager2_score GROUP BY name HAVING count(manager1_id) = ".$response['most_wins_number']);
+    while($row = mysqli_fetch_array($result)) 
+    {
+        if ($tempName == '') {
+            $tempName = $row['name'];
+        } else {
+            $tempName .= ', '.$row['name'];
+        }
+    }
+
+    $response['most_wins_manager'] = $tempName;
+
+    return $response;
+}
+
+function getProfileName($conn)
+{
+    $response = '';
+    if (isset($_GET)) {
+        $result = mysqli_query($conn, "SELECT * FROM managers WHERE id = ".$_GET['id']);
+        while($row = mysqli_fetch_array($result)) 
+        {
+            $response = $row['name'];
+        }
+    } else {
+        // redirect to index
+    }
+
+    return $response; 
+}
--- header.php
+++ header.php
@@ -43,14 +43,13 @@
             <div class="navbar-header">
                 <ul class="nav navbar-nav">
                     <li class="nav-item mobile-menu hidden-md-up float-xs-left"><a class="nav-link nav-menu-main menu-toggle hidden-xs"><i class="icon-menu5 font-large-1"></i></a></li>
-                    <li class="nav-item"><a href="index.html" class="navbar-brand nav-link"><img alt="branding logo" src="images/robust-logo-light.png" data-expand="images/robust-logo-light.png" data-collapse="images/robust-logo-small.png" class="brand-logo"></a></li>
+                    <li class="nav-item"><h2>Suntown FFB</h2></li>
                     <li class="nav-item hidden-md-up float-xs-right"><a data-toggle="collapse" data-target="#navbar-mobile" class="nav-link open-navbar-container"><i class="icon-ellipsis pe-2x icon-icon-rotate-right-right"></i></a></li>
                 </ul>
             </div>
             <div class="navbar-container content container-fluid">
                 <div id="navbar-mobile" class="collapse navbar-toggleable-sm">
-                    
-                    
+                    <h2><?php echo $pageName ?></h2>
                 </div>
             </div>
         </div>

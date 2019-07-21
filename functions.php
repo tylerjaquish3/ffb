c0b8990140e14c1f4606eb8addbea3e1d03c961e
Tyler Jaquish <tylerjaquish@gmail.com>
2019-07-20 19:00:47 -0700

    Added page for misc stats.

--- functions.php
+++ functions.php
@@ -7,7 +7,7 @@ $password = "";
 $dbname = "ffb";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname, '3307');
+$conn = new mysqli($servername, $username, $password, $dbname, '3306');
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);

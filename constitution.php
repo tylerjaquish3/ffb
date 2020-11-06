a0d7cd7893e71ffdc3b7d6cf9516335fc5c250c0
Tyler Jaquish <tjaquish@stepstoneglobal.com>
2020-11-06 09:37:05 -0800

    Added constitution.

---
+++ constitution.php
@@ -0,0 +1,150 @@
+<?php
+
+$pageName = "Constitution";
+include 'header.php';
+include 'sidebar.html';
+
+?>
+
+<div class="app-content content container-fluid">
+    <div class="content-wrapper">
+        <div class="content-header row"></div>
+
+        <div class="content-body">
+
+            <div class="row">
+                <div class="col-xs-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <h4>League Info</h4>
+                        </div>
+                        <div class="card-body">
+                            <div class="card-block" style="direction: ltr">
+                                <p>We, the members of Suntown Fantasy Football League, in order to promote and encourage interest in Fantasy Football,
+                                    do ordain and establish this Constitution for the government of our association, including the heathens that do not hail from Sunnyside, WA.
+                                </p>
+
+                                <p>The Suntown Fantasy Football league began many moons ago, as a group of college students that enjoyed watching football together.
+                                    The ten league members, as they stand today, began competing in 2008. However, the coveted league trophy dates back to 2006, where
+                                    the league only contained eight members. The data on this website dates back to 2006 to correlate with the league trophy.</p>
+
+                                <p>This league will always be a free, head-to-head league hosted by Yahoo. The only prize is a plaque with the champion's name inscribed that gets added to the league trophy,
+                                    as well as the trophy living with the champion until a new champion is crowned. The league has also voted that the 10th place finisher
+                                    must pay for the new plaque at the end of each season.</p>
+
+                                <p>The league matchups schedule shall use data from prior seasons to determine the optimal weekly schedules for each manager to face opponents as equally as possible.
+                                    Since this has been enacted relatively recently, the data is skewed and may take several seasons to even out the schedules.
+                                </p>
+
+                                <p>All parts of this constitution, including rules and practices, may be modified or ammended with a majority vote. However, if deemed minor, changes may be made by the commissioner without
+                                    whining or complaining by the members. Questions concerning the interpretation of this document shall be reviewed and <u>swiftly</u> deleted by the commissioner.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-xs-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <h4>Draft</h4>
+                        </div>
+                        <div class="card-body">
+                            <div class="card-block" style="direction: ltr">
+                                <p>The fantasy football draft is the single most important day of the fantasy football season, but the league is not won at the draft.
+                                Our draft is held on or around the end of August, generally one week before the kickoff of the NFL season.</p>
+
+                                <p>Approximately 6 weeks before the draft, the draft order is determined in a unique but random fashion. This is created by the commissioner
+                                and recorded for video evidence, with no retakes or edits in order to preserve authenticity and integrity. For instance, when the dog ran
+                                away with the tennis ball with Gavin's name on it, the video continued uncut. The draft order will always be determined with complete randomness,
+                                giving every manager an equal chance at getting the #1 pick.</p>
+
+                                <p>The draft shall always be of "snake" orientation and draft grades given by Yahoo shall always be considered 100% accurate and reliable.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-xs-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <h4>League Rules</h4>
+                        </div>
+                        <div class="card-body">
+                            <div class="card-block" style="direction: ltr">
+                                <ul>
+                                    <li>No collusion amongst teams in regards to trades or determining the outcome of a matchup. This includes intentional losing to effect the league standings.</li>
+                                    <li>No trade "rentals" where the same trade is reversed.</li>
+                                    <li>No trading if you're mathematically eliminated from postseason as this is considered collusion.</li>
+                                    <li>Trades have a 2 day period where they can be reviewed/vetoed by the league. Therefore, the deadline for a trade to take effect
+                                        for the current week is to be accepted by Thursday. If any trade elements play on Thursday, the deadline for the trade to be accepted is Monday.
+                                    </li>
+                                    <li>Smack talk is encouraged, especially if it hurts feelings.</li>
+                                    <li>Players added and dropped in the same day shall return to Free Agent status (not Waivers).</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-xs-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <h4>League Settings</h4>
+                        </div>
+                        <div class="card-body">
+                            <div class="card-block" style="direction: ltr">
+                                <ul>
+                                    <li>Waiver Time:	3 days</li>
+                                    <li>Waiver Type:	Continual rolling list</li>
+                                    <li>Weekly Waivers:	Game Time - Tuesday</li>
+                                    <li>Post Draft Players:	Follow Waiver Rules</li>
+                                    <li>Playoffs:	Week 14, 15, and 16 (6 teams)</li>
+                                    <li>Playoff Tie-Breaker:	Best regular season record vs opponent wins</li>
+                                    <li>Playoff Reseeding:	Yes</li>
+                                    <li>Divisions:	No</li>
+                                    <li>Roster Positions:	QB, WR, WR, WR, RB, RB, TE, W/R/T, Q/W/R/T, K, DEF, BN, BN, BN, BN, BN, BN, IR, IR, IR, IR</li>
+                                    <li>Fractional Points:	Yes</li>
+                                    <li>Negative Points:	Yes</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-xs-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <h4>Vote</h4>
+                        </div>
+                        <div class="card-body">
+                            <div class="card-block" style="direction: ltr">
+                                Before the start of next season, the league shall vote on the following:
+                                <ul>
+                                    <li>Team Defense points awarded</li>
+                                    <li>Include/exclude IDP positions</li>
+                                    <li>Keep the superflex position (Q/W/R/T)</li>
+                                    <li>Keep 4 IR positions</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
+
+<?php include 'footer.html'; ?>
+
+<script type="text/javascript">
+
+</script>

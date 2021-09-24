56797c1fcdfde3ecd8dec863891540a8c7c421c0
Tyler Jaquish <tyler.jaquish@stepstonegroup.com>
2021-09-24 10:27:10 -0700

    Added 404 page

---
+++ 404.php
@@ -0,0 +1,34 @@
+<?php
+
+$pageName = "File Not Found";
+include 'header.php';
+include 'sidebar.html';
+
+?>
+
+<div class="app-content content container-fluid">
+    <div class="content-wrapper">
+        <div class="content-header row"></div>
+
+        <div class="content-body">
+
+            <div class="row">
+                <div class="col-xs-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <h4></h4>
+                        </div>
+                        <div class="card-body">
+                            <div class="card-block" style="direction: ltr">
+                                <h3>Congratulations, you broke the internet.</h3>
+                                Sorry bro, looks like you're lost. This page doesn't exist. 
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div> 
+
+        </div>
+    </div>
+</div>
+

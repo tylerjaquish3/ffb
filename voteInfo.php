7899ff2682a7ea8d67ff4f03cfcabe2f098ca347
Tyler Jaquish <tyler.jaquish@stepstonegroup.com>
2022-07-18 14:40:03 -0700

    Updating vote info

--- voteInfo.php
+++ voteInfo.php
@@ -11,26 +11,24 @@ include 'sidebar.html';
 
         <div class="content-body">
 
-            <div class="row" id="superflex">
+            <div class="row" id="waivers">
                 <div class="col-sm-12 table-padding">
                     <div class="card">
                         <div class="card-header">
-                            <h4 class="card-title">Roster Superflex Info</h4>
+                            <h4 class="card-title">Continual Rolling List Waivers vs FAB Info</h4>
                         </div>
                         <div class="card-body info-card">
-                            2019: 2 QB spots<br />
-                            2020: 1 QB spot, 1 Q/W/R/T spot<br />
-                            Superflex was added to provide more flexibility for a season that had many unknowns (covid).<br />
-                            Throughout the season, the flexibility was utilized a total of 4 times:
-                            <ul>
-                                <li>Tyler wk 4 - wasn't forced to, and didn't work out beneficially</li>
-                                <li>Ben wk 6 - forced to based on roster make up and byes</li>
-                                <li>Tyler wk 6 - forced to based on roster make up and injury</li>
-                                <li>Everett wk 13 - wasn't forced to, and didn't work out beneficially</li>
-                            </ul>
-                            It seemed that if QBs were effected by covid, the manager was able to pivot to another QB in all cases.
-                            Also, it appears that the 2021 season has fewer unknowns than 2020.<br />
-                            <b>Proposal:</b> Change the Q/W/R/T position back to a QB position.
+                            FAB stands for Free Agent Acquisition Budget (Yahoo calls it FAB)<br />
+                            In a Free Agents Budgets (FAB) waivers system, each manager receives a dollar amount to place blind offers on waived players throughout the season. 
+                            The manager with the highest offer at the end of the waiver period claims that player, and that offer amount is deducted from the team's free agent budget for the season. If multiple managers place an equal offer on a player, tiebreak options are available as described above.
+                            <br />
+                            We could start with any amount of FAB budget, standard is $100 or $200, with the option to also place $0 bids.
+                            <br />
+                            The benefit to using the FAB system is that it gives everyone a chance to acquire a waiver player, and will be determined by who
+                            is willing to be the most aggressive with their budget.
+                            <br />
+                            
+                            <b>Proposal:</b> Change the Waiver Priority setting to FAB with reverse order of standings as tiebreak, budget set to $200.
                         </div>
                     </div>
                 </div>
@@ -42,21 +40,15 @@ include 'sidebar.html';
                             <h4 class="card-title">Roster IR Info</h4>
                         </div>
                         <div class="card-body info-card">
-                            2019: 0 IR spots<br />
+                            
                             2020: 4 IR spots<br />
                             2021: 2 IR spots<br />
                             The NFL's IR designation changed in 2020 to not be a season-ending or 8-week designation, but can now be as few as 3 weeks.
-                            IR spots were added in our fantasy league to provide more flexibility for a season that had many unknowns (covid).<br />
-                            2020 numbers (reg. season):<br />
-                            <ul>
-                                <li>4 IR spots used three times in a week (Justin wk 3, Cole wk 12, Cole wk 13)</li>
-                                <li>3 IR spots used twelve times</li>
-                                <li>Total weeks = 130 (10 managers for 13 reg. season weeks)</li>
-                                <li>Therefore, the extra spots were used 15 times in 130 weeks</li>
-                            </ul>
-                            We don't have exact numbers on how many IR uses were related to covid, but best guess is less than 10%.
-                            Regardless, the IR spot is useful because more players are put on real life IR and return in three weeks.
-                            It helps managers not have to use bench spots for injured players. <br />
+                            IR spots were added in our fantasy league to provide more flexibility for a season that had many unknowns.<br />
+                            The IR spot is useful because more players are put on real life IR and return in three weeks.
+                            It helps managers not have to use bench spots for injured players and therefore have better rosters. <br />
+                            The argument against the IR spot is to make managers decide if the injured player is worth stashing (taking up a bench spot) while on IR.
+                            This could create more activity for waivers.
 
                             <b>Proposal:</b> Change the number of IR spots to 1 or 0.
                         </div>
@@ -69,39 +61,3 @@ include 'sidebar.html';
 </div>
 
 <?php include 'footer.html'; ?>
-
-<script type="text/javascript">
-    $(document).ready(function () {
-
-
-        $('#datatable-wins').DataTable({
-            "searching": false,
-            "paging": false,
-            "info": false,
-            "order": [
-                [3, "desc"]
-            ]
-        });
-
-        // Misc tables
-        $('#datatable-misc1').DataTable({
-            "searching": false,
-            "paging": false,
-            "info": false,
-            "order": [
-                [1, "desc"]
-            ]
-        });
-        $('#datatable-misc2').DataTable({
-            "searching": false,
-            "paging": false,
-            "info": false,
-            "order": [
-                [1, "desc"]
-            ]
-        });
-
-    });
-
-
-</script>

86c3590da39918e591b77b1fb443205587713bb4
Tyler Jaquish <tyler.jaquish@stepstonegroup.com>
2023-11-02 08:20:46 -0700

    Updated vote info

--- voteInfo.php
+++ voteInfo.php
@@ -34,66 +34,83 @@ include 'sidebar.html';
                     </div>
                 </div>
             </div>
-            <!-- <div class="row" id="kicker-points">
+            <div class="row" id="trade_voting">
                 <div class="col-sm-12 table-padding">
                     <div class="card">
                         <div class="card-header">
-                            <h4 class="card-title">Remove Negative Kicker Points</h4>
+                            <h4 class="card-title">Push trades through right away</h4>
                         </div>
                         <div class="card-body info-card">
-                            Current Settings: <br />
+                            We have never seen collusion in our league, and it doesn't seem like it will ever happen - everyone wants to win badly (although Gavin's track record
+                            doesn't show it) and there is no prize to share.<br /><br />
+                            So the question is, why would the league veto a trade? Every accepted trade is between two consenting adults (insert sexual joke here) that are
+                            attempting to improve their team. The opinions of other league members about the involved players' values shouldn't matter, there is always risk in
+                            any trade. So why do we have a 3 day window in which the league can vote to veto? This has been in place since league inception, but does it make sense?
+                            The 3 day window can throw a wrench in the system, where accepted trades may not process until after a player has played and possibly been injured. That 
+                            scenario makes Friday-Sunday trades not likely to happen, and if the trade involves any Thursday games, it makes Tuesday-Sunday trades not likely.
+                            <br />
+                            <br />
+                            The alternative is to allow the commissioner to push trades through as soon as they are agreed upon, or as soon as the commissioner can get to it. This
+                            would allow trades to go through the same day so you can prepare for the week or make last minute moves up to game time. If, in the unlikely scenario
+                            that the league has a serious qualm about the trade, the league may vote to undo it.
+                            <br />
+                            <br />
+                            <b>Proposal:</b> Allow commissioner to push trades through right away.<br />
+                            <i>Note: we may want to assign a co-commissioner for the sole purpose of pushing trades through in the event that the commissioner is unavailable.</i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row" id="punishment">
+                <div class="col-sm-12 table-padding">
+                    <div class="card">
+                        <div class="card-header">
+                            <h4 class="card-title">Replacing toilet seat as punishment</h4>
+                        </div>
+                        <div class="card-body info-card">
+                            In 2021, we voted to add punishment for the last place team, where they have to sit on a toilet during the draft and their team is named
+                            by the defending champion (the loser also pays for the plaque to be added to the trophy). The toilet was a funny prop for the first draft 
+                            or two, but it really lost its luster to where it had almost no effect in last year's draft. 
+                            <br />
+                            <br />
+                            The question is whether we should just remove the toilet punishment or replace it with some other punishment.
+                            <br />
+                            <br />
+                            <b>Proposal A:</b> Remove toilet seat as a punishment.<br />
+                            <b>Proposal B:</b> Replace toilet punishment with a different punishment. Options:
                             <ul>
-                                <li>Missed FG 0-19 yds   :   -5 pts</li>
-                                <li>Missed FG 20-29 yds  :   -4 pts</li>
-                                <li>Missed FG 30-39 yds  :   -2 pts</li>
-                            </ul>
-                            Currently, the negative points for missed field goals are too drastic for what they are worth if made. For instance, a 19 yard field goal is worth 1.9 points, but costs -5 if missed.
-                            That equates to a 6.9 point swing for a short field goal missed, which is one of the largest point changes in our league. 
-                            <br />
-                            <br />
-                            The reason we set it this way was to make kickers more impactful and base the negative points on the likelihood of making the field goal and how often it occurs.
-                            <br />
-                            <br />
-                            <b>Proposal:</b>
-                            <ul>
-                                <li>0-19: -2 pts</li>
-                                <li>20-29: -1 pts</li>
-                                <li>30-39: -1 pts</li>
+                                <li>Loser gives PowerPoint presentation explaining why they lost.</li>
+                                <li>Giving the winner a relaxing massage.</li>
+                                <li>Polar plunge.</li>
+                                <li>Body waxing.</li>
+                                <li>Winner can choose a custom punishment for the loser each year.</li>
+                                <li>Other ?</li>
                             </ul>
                         </div>
                     </div>
                 </div>
-            </div> -->
-            <!-- <div class="row" id="waivers">
+            </div>
+            <div class="row" id="trade_fav">
                 <div class="col-sm-12 table-padding">
                     <div class="card">
                         <div class="card-header">
-                            <h4 class="card-title">Waiver Info</h4>
+                            <h4 class="card-title">Ability to trade FAB dollars</h4>
                         </div>
                         <div class="card-body info-card">
-                            
-                            Current Settings: <br />
-                            <ul>
-                                <li>Waiver Time: 3 days</li>
-                            </ul>
-                            Currently, players stay on waivers for 3 days once being dropped and that doesn't allow players to be added for the following week in the typical add/drop cycle. 
-                            For instance, on Waiver day (Tuesday) people put in waivers and overnight those waivers are processed. So in this typical cycle, if a player is dropped technically on Wednesday, 
-                            they are on waivers until Sunday morning. In some cases, that makes this player ineligible to be added for the following week (if they play Thursday).
+                            There may be times where a trade is very close to even, but one trade partner wants slightly more in return. Allowing trades to include FAB money could
+                            help more trades go through, where a manager may be willing to give up a little more in FAB money to get the trade done. This would also allow managers
                             <br />
                             <br />
-                            Sometimes a player is held and dropped at a certain time in order to block other managers from getting them, which is fine and valid. This rule would just extend the time
-                            that a manager would have to keep the player. For instance, if you dropped a player on Thursday, they would be on waivers until Monday morning. If this new rule was in place,
-                            the player would have to be kept until Friday in order to block other managers from adding them for Sunday.
+                            The downside to this is that Yahoo doesn't have this included in their trade system, so it would have to be done manually. The trade partners would have 
+                            to agree on the FAB money in a separate chat and the commissioner would have to make the changes manually in Yahoo.
                             <br />
                             <br />
-                            <b>Proposal:</b>
-                            <ul>
-                                <li>Waiver Time: 2 days</li>
-                            </ul>
+                            <b>Proposal:</b> Allow managers to include FAB money in trades.<br />
                         </div>
                     </div>
                 </div>
-            </div> -->
+            </div>
+            
             
         </div>
     </div>

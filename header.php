aeabcc685d5ec9124b0cc511e889347c8ee22627
Tyler Jaquish <tylerjaquish@gmail.com>
2023-12-12 19:44:22 -0800

    Changes for mysql

--- header.php
+++ header.php
@@ -7,8 +7,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
 
     <title>Suntown FFB</title>
-    <?php $version = "v2.4.0"; 
-    $vDate = "(12/8/23)"; ?>
+    <?php $version = "v2.4.1"; 
+    $vDate = "(12/12/23)"; ?>
 
     <link rel="icon" type="image/png" href="/images/football.ico">
 

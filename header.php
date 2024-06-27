836b8fbadd7bf93e08424672d2a616c95e894898
Tyler Jaquish <tyler.jaquish@stepstonegroup.com>
2024-06-27 13:19:22 -0700

    Updated version

--- header.php
+++ header.php
@@ -7,8 +7,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
 
     <title>Suntown FFB</title>
-    <?php $version = "v2.5.0"; 
-    $vDate = "(6/14/24)"; ?>
+    <?php $version = "v2.5.1"; 
+    $vDate = "(6/27/24)"; ?>
 
     <link rel="icon" type="image/png" href="/images/football.ico">
 

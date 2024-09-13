5cbf7c1b5f1162280629be2eb84ad16bcb1c3cb7
Tyler Jaquish <tyler.jaquish@stepstonegroup.com>
2024-09-13 11:06:41 -0700

    Filter by new leader and added page name to title

--- awards.php
+++ awards.php
@@ -8,6 +8,11 @@ $type = 'all';
 if (isset($_GET['id'])) {
     $type = $_GET['id'];
 } 
+if (isset($_GET['new_leader'])) {
+    $new_leader = $_GET['new_leader'];
+} else {
+    $new_leader = 0;
+}
 ?>
 
 <div class="app-content content container-fluid">
@@ -19,13 +24,20 @@ if (isset($_GET['id'])) {
                 <div class="col-sm-12 d-md-none">
                     <h5 style="margin-top: 5px; color: #fff;">Filter</h5>
                 </div>
-                <div class="col-sm-12 col-md-4">
-                    <select id="type-select" class="form-control">
+                <div class="col-sm-12 col-md-6" style="display: flex;">
+                    &nbsp;&nbsp;
+                    <h4>Season Type</h4>
+                    &nbsp;&nbsp;
+                    <select id="type-select">
                         <option value="all" <?php if ($type == 'all') { echo 'selected'; } ?>>All</option>
                         <option value="regular" <?php if ($type == 'regular') { echo 'selected'; } ?>>Regular Season</option>
                         <option value="post" <?php if ($type == 'post') { echo 'selected'; } ?>>Postseason</option>
                         <option value="current" <?php if ($type == 'current') { echo 'selected'; } ?>>Current Season</option>
                     </select>
+                    &nbsp;&nbsp;
+                    <h4>New Leader</h4>
+                    &nbsp;&nbsp;
+                    <input type="checkbox" id="new_leader" <?php echo $new_leader == 1 ? 'checked' : '' ?>>
                 </div>
             </div>
 
@@ -56,6 +68,9 @@ if (isset($_GET['id'])) {
                                             JOIN managers ON managers.id = mff.manager_id
                                             WHERE is_positive = 1 AND manager_id = $x";
 
+                                        if ($new_leader) {
+                                            $query .= " AND new_leader = 1";
+                                        }
                                         if ($type != 'all') {
                                             $query .= " AND type = '$type'";
                                         }
@@ -81,6 +96,9 @@ if (isset($_GET['id'])) {
                                             JOIN managers ON managers.id = mff.manager_id
                                             WHERE is_positive = 0 AND manager_id = $x";
                                             
+                                        if ($new_leader) {
+                                            $query .= " AND new_leader = 1";
+                                        }
                                         if ($type != 'all') {
                                             $query .= " AND type = '$type'";
                                         }
@@ -117,6 +135,13 @@ if (isset($_GET['id'])) {
     let baseUrl = "<?php echo $BASE_URL; ?>";
 
     $('#type-select').change(function() {
-        window.location = baseUrl+'awards.php?id='+$('#type-select').val();
+        newLeader = $('#new_leader').is(':checked') ? 1 : 0;
+        window.location = baseUrl+'awards.php?id='+$('#type-select').val()+'&new_leader='+newLeader;
     });
+
+    $('#new_leader').change(function() {
+        newLeader = $('#new_leader').is(':checked') ? 1 : 0;
+        window.location = baseUrl+'awards.php?id='+$('#type-select').val()+'&new_leader='+newLeader;
+    });
+
 </script>

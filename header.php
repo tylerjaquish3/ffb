54e14a562655fa34e5454a9de4262ecddb00a206
Tyler Jaquish <tyler.jaquish@stepstonegroup.com>
2024-12-31 09:30:42 -0700

    Added end of 2024 data

--- header.php
+++ header.php
@@ -7,8 +7,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
 
     <title><?php echo isset($pageName) ? $pageName. ' | Suntown FFB' : 'Suntown FFB'; ?></title>
-    <?php $version = "v2.6.4"; 
-    $vDate = "(12/9/24)"; ?>
+    <?php $version = "v2.6.5"; 
+    $vDate = "(12/31/24)"; ?>
 
     <link rel="icon" type="image/png" href="/images/football.ico">
 
